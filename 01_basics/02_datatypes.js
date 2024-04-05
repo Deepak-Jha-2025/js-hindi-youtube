a71872e75bc8ddd4f4fa49a18578e2428cde037c
repodeep <129058395+repodeep@users.noreply.github.com>
2024-04-05 19:21:24 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict" // treat all JS code as newer version of JS
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+let name = "hitesh"
+let age = 18 
+let isLoggedIn = false
+let state;
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned yet
+// symbol => uniqueness
+
+
+// object 
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
